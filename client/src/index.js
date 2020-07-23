cde3f5960538a660419270afe8a605d31a95de0a
Martinez Herrera Marlon Giovanni <martinez.1ded@gmail.com>
2020-07-23 15:38:24 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,19 +4,14 @@ import './index.scss';
 import App from './App';
 
 if (process.env.NODE_ENV === 'production') {
-	localStorage.setItem('Clip', 'http://localhost:8080/');
-	localStorage.setItem(
-		'URL',
-		'http://localhost:8080/api/v1/'
-	); /* 
 	localStorage.setItem('Clip', 'https://young-lowlands-83239.herokuapp.com/');
 	localStorage.setItem(
 		'URL',
 		'https://young-lowlands-83239.herokuapp.com/api/v1/'
-	); */
+	);
 } else {
-	localStorage.setItem('Clip', 'http://localhost:8080/');
-	localStorage.setItem('URL', 'http://localhost:8080/api/v1/');
+	localStorage.setItem('Clip', 'http://localhost:5000/');
+	localStorage.setItem('URL', 'http://localhost:5000/api/v1/');
 }
 ReactDOM.render(
 	<React.StrictMode>
